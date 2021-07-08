807dc1f05632beaeacfc191c750736eb21cb5cda
Suvamjyoti Panda <spanda@xansrtech.com>
2021-07-08 08:59:12 +0530

    character movement setup

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -7,9 +7,19 @@ public class PlayerController : MonoBehaviour
     private Rigidbody characterRB;
     private float horizontal;
 
+    [SerializeField] private Transform leftPosition;
+    [SerializeField] private Transform centerPosition;
+    [SerializeField] private Transform rightPosition;
+
+    private playerPositionState currentPosition;
+
+    private enum playerPositionState { left,right,center}
+
     private void Start()
     {
         characterRB = this.GetComponent<Rigidbody>();
+        transform.position = centerPosition.position;
+        currentPosition = playerPositionState.center;
     }
 
     private void FixedUpdate()
@@ -19,6 +29,39 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        if(currentPosition != playerPositionState.left)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = leftPosition.position;
+
+                if (currentPosition == playerPositionState.center)
+                {
+                    currentPosition = playerPositionState.left;
+                }
+                else
+                {
+                    currentPosition = playerPositionState.center;
+                }
+            }
+        }
+
+        if (currentPosition != playerPositionState.right)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = rightPosition.position;
+
+                if (currentPosition == playerPositionState.center)
+                {
+                    currentPosition = playerPositionState.right;
+                }
+                else
+                {
+                    currentPosition = playerPositionState.center;
+                }
+            }
+        }
+        
     }
 }
